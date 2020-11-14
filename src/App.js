c113971a8f41c469e1fbd83c2a92632f18b4e41c
Emanoel Queiroz <emanoelqueiroz@gmail.com>
2020-11-14 01:47:26 -0300

    adicionado titulo da página

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import PageTitle from './components/PageTitle';
 import List from './components/Stock/List';
 
 function App() {
   return (
     <div className="App">
+      <PageTitle>Controle de Estoque</PageTitle>
       <List/>
     </div>
   );
---
+++ src/components/PageTitle.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+import React, { Component } from "react";
+import Section from './Section';
+
+const TitleWrapper = styled.div`
+    background-color: #785afc;
+    padding: 21px 0;
+`;
+
+const Title = styled.h1`
+    color: #342b82;
+    margin: 0;
+    font-size: 34px;
+`;
+
+class PageTitle extends Component {
+    render() {
+        return (
+            <TitleWrapper>
+                <Section>
+                    <Title>
+                        {this.props.children}
+                    </Title>
+                </Section>
+            </TitleWrapper>
+        )
+    }
+}
+
+export default PageTitle
